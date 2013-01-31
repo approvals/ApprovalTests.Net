dc00e08a00b8fc6a847d2ec78874cd52b2de0a20
Llewellyn Falco <isidore@setgame.com>
2013-01-31 12:55:31 -0600

    language portable (cls compliant)

--- ApprovalUtilities/Properties/AssemblyInfo.cs
+++ ApprovalUtilities/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -12,6 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
 [assembly: AssemblyKeyFileAttribute("..\\ApprovalsKeyPair.snk")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
