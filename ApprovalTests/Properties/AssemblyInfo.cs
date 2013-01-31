b80d8b98c9120d665ff75db7138ede250306e35c
Llewellyn Falco <isidore@setgame.com>
2013-01-31 13:02:04 -0600

    CLS complaint

--- ApprovalTests/Properties/AssemblyInfo.cs
+++ ApprovalTests/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -13,6 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
 [assembly: AssemblyKeyFileAttribute("..\\ApprovalsKeyPair.snk")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
