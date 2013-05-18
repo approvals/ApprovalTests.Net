583ffab0fdae855a886d6a20e02e9c15b3b13f9d
Llewellyn Falco <isidore@setgame.com>
2013-05-17 19:29:33 -0500

    Xunit support

--- ApprovalTests.Tests/Namer/XunitStackTraceNamerTest.cs
+++ ApprovalTests.Tests/Namer/XunitStackTraceNamerTest.cs
@@ -26,21 +26,30 @@ namespace ApprovalTests.Tests.Namer
 			Approvals.Verify(exception.Message);
 		}
 
-        [Fact]
-        public async Task AsyncTestApprovalName()
-        {
-            var name = new UnitTestFrameworkNamer().Name;
-            var path = new UnitTestFrameworkNamer().SourcePath;
+		[Fact]
+		public async Task AsyncTestApprovalName()
+		{
+			var name = new UnitTestFrameworkNamer().Name;
+			var path = new UnitTestFrameworkNamer().SourcePath;
 
-            await AnAsyncMethod();
+			await AnAsyncMethod();
 
-            Assert.Equal("XunitStackTraceNamerTest.AsyncTestApprovalName", name);
-            Assert.True(File.Exists(path + "\\XunitStackTraceNamerTest.cs"));
-        }
+			Assert.Equal("XunitStackTraceNamerTest.AsyncTestApprovalName", name);
+			Assert.True(File.Exists(path + "\\XunitStackTraceNamerTest.cs"));
+		}
+		
+		[Fact]
+		public async Task FullAsyncTest()
+		{
+			await AnAsyncMethod();
 
-	    private static Task AnAsyncMethod()
-	    {
-	        return TaskEx.FromResult(default(object));
-	    }
+			Approvals.Verify("Async");
+		}
+
+
+		private static Task AnAsyncMethod()
+		{
+			return TaskEx.FromResult(default(object));
+		}
 	}
 }
