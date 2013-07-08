d1509d037c9735ee4d0c485237b37e76b1a47bf3
Llewellyn Falco <isidore@setgame.com>
2013-07-08 18:07:57 -0500

    v 3.0

--- CreateNuget.cmd
+++ CreateNuget.cmd
@@ -1,4 +1,4 @@
-set approval_test_version=1.22.1
+set approval_test_version=3.0.01
 del /Q .\nuget_packages\*.*
 .nuget\NuGet.exe pack ApprovalUtilities\ApprovalUtilities.CSharp.nuspec -OutputDirectory .\nuget_packages -Version %approval_test_version%
 .nuget\NuGet.exe pack ApprovalTests\ApprovalTests.CSharp.nuspec -OutputDirectory .\nuget_packages -Version %approval_test_version%
--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 
-[assembly: AssemblyVersion("1.22")]     // Don't change this
-[assembly: AssemblyFileVersion("1.22.1")] // Change this
+[assembly: AssemblyVersion("3.0")]     // Don't change this
+[assembly: AssemblyFileVersion("3.0.01")] // Change this
