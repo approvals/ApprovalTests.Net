f59ecdb8b2871a4cefd0fd1721e762f0c6a05b56
michaelferrante <ferrante@aracnet.com>
2013-07-25 18:37:51 -0700

    Adding a CruiseControlNetReporter in the same pattern as TeamCityReporter so that by default when running under CruiseControl.NET the ApprovalTests will be in silent mode.

---
+++ ApprovalTests/Reporters/CruiseControlNetReporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ApprovalTests.Core;
+
+namespace ApprovalTests.Reporters
+{
+	public class CruiseControlNetReporter : IEnvironmentAwareReporter
+	{
+		public static readonly CruiseControlNetReporter INSTANCE = new CruiseControlNetReporter();
+
+		public void Report(string approved, string received)
+		{
+			// does nothing
+		}
+
+		public bool IsWorkingInThisEnvironment(string forFile)
+		{
+            return Environment.GetEnvironmentVariable("CCNetLabel") != null;
+		}
+	}
+}
