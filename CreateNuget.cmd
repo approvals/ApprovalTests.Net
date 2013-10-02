850d1c9b0fc75e0ed051409cd1de2846e03938f8
Llewellyn Falco <isidore@setgame.com>
2013-10-01 19:51:32 -0500

    better nuget script w/jim counts

--- CreateNuget.cmd
+++ CreateNuget.cmd
@@ -1,4 +1,5 @@
-set approval_test_version=3.0.01
+set approval_test_version=3.0.2
+if not exist .\nuget_packages mkdir nuget_packages
 del /Q .\nuget_packages\*.*
 .nuget\NuGet.exe pack ApprovalUtilities\ApprovalUtilities.CSharp.nuspec -OutputDirectory .\nuget_packages -Version %approval_test_version%
 .nuget\NuGet.exe pack ApprovalTests\ApprovalTests.CSharp.nuspec -OutputDirectory .\nuget_packages -Version %approval_test_version%
