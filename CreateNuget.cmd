d0407afd3f4d9a659307d25879036ffd96f1312d
Llewellyn Falco <isidore@setgame.com>
2013-10-01 19:52:49 -0500

    Version 3.0.2

--- CreateNuget.cmd
+++ CreateNuget.cmd
@@ -3,4 +3,4 @@ if not exist .\nuget_packages mkdir nuget_packages
 del /Q .\nuget_packages\*.*
 .nuget\NuGet.exe pack ApprovalUtilities\ApprovalUtilities.CSharp.nuspec -OutputDirectory .\nuget_packages -Version %approval_test_version%
 .nuget\NuGet.exe pack ApprovalTests\ApprovalTests.CSharp.nuspec -OutputDirectory .\nuget_packages -Version %approval_test_version%
-pause
+pause 
