cd242cf6f184cdd4ef4451866225479a0331eb0e
Llewellyn Falco <isidore@setgame.com>
2013-11-14 16:00:49 -0600

    Add to approval utitilies

---
+++ ApprovalUtilities.Tests/MaintenanceTest.cs
@@ -0,0 +1,15 @@
+﻿using ApprovalTests.Maintenance;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApprovalUtilities.Tests
+{
+	[TestClass]
+	public class MaintenanceTest
+	{
+		[TestMethod]
+		public void EnsureNoAbandonedFiles()
+		{
+			ApprovalMaintenance.VerifyNoAbandonedFiles();
+		}
+	}
+}
