8ab015f576dde765fd7a021b9e08326618b08b8f
Llewellyn Falco <isidore@setgame.com>
2013-11-19 16:26:23 -0600

    cruft...

--- MvcApplication.Razor/Views/Config.cs
+++ MvcApplication.Razor/Views/Config.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace MvcApplication1.Views
+﻿namespace MvcApplication1.Views
 {
     public class Config
     {
