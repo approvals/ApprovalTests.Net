c15c012d303232b232a478fc25b82f2a99e3b884
Llewellyn Falco <isidore@setgame.com>
2013-11-26 12:16:35 -0600

    Error Msg for bad async w/george

--- ApprovalTests/Namers/UnitTestFrameworks/XUnitStackTraceParser.cs
+++ ApprovalTests/Namers/UnitTestFrameworks/XUnitStackTraceParser.cs
@@ -1,3 +1,12 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using ApprovalTests.Reporters;
+using ApprovalUtilities.CallStack;
+using ApprovalUtilities.Utilities;
+
 namespace ApprovalTests.StackTraceParsers
 {
 	public class XUnitStackTraceParser : AttributeStackTraceParser
@@ -13,5 +22,44 @@ namespace ApprovalTests.StackTraceParsers
 		{
 			return Attribute;
 		}
+
+		protected override Caller FindApprovalFrame()
+		{
+			return VerifyAsyncUsedProperly(base.FindApprovalFrame());
+		}
+
+		private static Caller VerifyAsyncUsedProperly(Caller res)
+		{
+			if (res == null)
+			{
+				return res;
+			}
+			var asyncFrame = res.GetFirstFrameForAttribute<AsyncStateMachineAttribute>();
+			MethodBase method = res.Method;
+			var mi = method as MethodInfo;
+			if (asyncFrame != null && mi != null && mi.ReturnType == typeof (void))
+			{
+				var msg =
+					@"WARNING: You are using xUnit async incorrectly.
+The method {0} has a return type of void instead of Task.
+In this form this test can never fail, it will always pass.
+".FormatWith(method.ToStandardString());
+				msg = StringUtils.FormatFrame('*', msg);
+				FailLoudly(msg);
+			}
+			return res;
+		}
+
+		private static void FailLoudly(string msg)
+		{
+			Console.WriteLine(msg);
+			Debug.WriteLine(msg);
+			var tempFile = Path.ChangeExtension(Path.GetTempFileName(), "txt");
+			File.WriteAllText(tempFile, msg);
+			new FileLauncherReporter().Report(null, tempFile);
+			throw new InvalidOperationException(msg);
+		}
 	}
+}
+
 }
