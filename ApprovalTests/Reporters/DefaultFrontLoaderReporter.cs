8c0aadd6c16b8dcff746917bb826ad8c83f49942
Jim Counts <jamesrcounts@outlook.com>
2014-02-28 13:33:01 -0800

    Created TFS FrontLoaded Reporter

--- ApprovalTests/Reporters/DefaultFrontLoaderReporter.cs
+++ ApprovalTests/Reporters/DefaultFrontLoaderReporter.cs
@@ -6,6 +6,7 @@
 
         public DefaultFrontLoaderReporter()
             : base(
+            TfsReporter.INSTANCE,
             TeamCityReporter.INSTANCE,
             CruiseControlNetReporter.INSTANCE,
             NCrunchReporter.INSTANCE,
