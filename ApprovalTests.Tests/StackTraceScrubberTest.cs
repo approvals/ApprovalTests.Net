d34c1feea78af24abcd61cead85bf36f27b6cbf0
Llewellyn Falco <isidore@setgame.com>
2014-03-13 16:26:59 -0500

    Added Test w/jim

---
+++ ApprovalTests.Tests/StackTraceScrubberTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace ApprovalTests.Tests
+{
+	[TestFixture]
+	public class StackTraceScrubberTest
+	{
+		[Test]
+		public void TestDashedPath()
+		{
+			var path = @"C:\code\ApprovalTests - Net\Persistence\Datasets\DatasetTest.cs";
+			Assert.AreEqual("...\\DatasetTest.cs", StackTraceScrubber.ScrubPaths(path));
+		}
+	}
+}
