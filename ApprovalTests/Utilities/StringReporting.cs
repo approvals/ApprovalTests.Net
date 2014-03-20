009719d1bb10e990d800998fa552657b1d92103d
Llewellyn Falco <isidore@setgame.com>
2014-03-20 18:58:34 -0500

    Added String.DiffWith w/jim & jason

---
+++ ApprovalTests/Utilities/StringReporting.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace ApprovalTests.Utilities
+{
+	public static class StringReporting
+	{
+		public static void DiffWith(this string expected, string actual)
+		{
+			if (expected != actual)
+			{
+				var expectedFile = Path.GetTempPath() + "Expected.Approvals.Temp.txt";
+				var actualFile = Path.GetTempPath() + "Actual.Approvals.Temp.txt";
+
+				File.WriteAllText(expectedFile, expected);
+				File.WriteAllText(actualFile, actual);
+
+				var reporter = Approvals.GetReporter();
+				reporter.Report(expectedFile, actualFile);
+			}
+		}
+	}
+}
