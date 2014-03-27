692bdc16f9a8180fd5c6832c97f0dbe6f2515e54
Llewellyn Falco <isidore@setgame.com>
2014-03-27 14:01:44 -0500

    facory

---
+++ ApprovalTests/Writers/WriterFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ApprovalTests.Core;
+
+namespace ApprovalTests.Writers
+{
+	public class WriterFactory
+	{
+		private static Func<string, IApprovalWriter> TextWriterCreator = (s) => new ApprovalTextWriter(s);
+		public static void SetTextWriterCreator(Func<String, IApprovalWriter> textWriterCreator)
+		{
+			TextWriterCreator = textWriterCreator;
+		}
+		public static IApprovalWriter CreateTextWriter(String data)
+		{
+			return TextWriterCreator(data);
+		}
+		private static Func<string,string, IApprovalWriter> TextWriterWithExtensionCreator = (s,e) => new ApprovalTextWriter(s,e);
+		public static void SetTextWriterCreator(Func<String, String, IApprovalWriter> textWriterWithExtensionCreator)
+		{
+			TextWriterWithExtensionCreator = textWriterWithExtensionCreator;
+		}
+		public static IApprovalWriter CreateTextWriter(string data, string extensionWithoutDot)
+		{
+			return TextWriterWithExtensionCreator(data, extensionWithoutDot);
+		}
+	}
+}
