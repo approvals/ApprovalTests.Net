d3982ee5c501d3ed826ed74181ac147e386b9d56
Llewellyn Falco <isidore@setgame.com>
2014-04-03 14:03:27 -0500

    Added StatePrinterApprovals via Kasper

---
+++ ApprovalTests.Tests/StatePrinter/StatePrinterTests.cs
@@ -0,0 +1,29 @@
+﻿using ApprovalTests.StatePrinter;
+using NUnit.Framework;
+
+namespace ApprovalTests.Tests.StatePrinter
+{
+	[TestFixture]
+	public class StatePrinterTests
+	{
+		[Test]
+		public void TestVerifyCircleReferences()
+		{
+			var turtle = new Turtle("jim", new Turtle("kasper", new Turtle("llewellyn", null)));
+			turtle.On.On.On = turtle;
+			StatePrinterApprovals.Verify(turtle);
+		}
+	}
+
+	public class Turtle
+	{
+		public string Name { get; set; }
+		public Turtle On { get; set; }
+
+		public Turtle(string name, Turtle onTurtle)
+		{
+			Name = name;
+			On = onTurtle;
+		}
+	}
+}
