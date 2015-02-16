b45c7922898b3216ed61323c920d3b90333a27d1
Jim Counts <fritzjob18@nethere.com>
2015-02-16 10:30:42 -0800

    Added ApprovalUtilities.Net45 to myget build script

--- MyGet.cmd
+++ MyGet.cmd
@@ -1,3 +1,4 @@
 %NuGet% restore ApprovalTests.sln -NoCache -NonInteractive
 %NuGet% pack ApprovalUtilities\ApprovalUtilities.csproj -OutputDirectory .\ApprovalUtilities -Symbols -Build -Properties Configuration=Debug -Version %PackageVersion%
+%NuGet% pack ApprovalUtilities.Net45\ApprovalUtilities.Net45.csproj -OutputDirectory .\ApprovalUtilities.Net45 -Symbols -Build -Properties Configuration=Debug -Version %PackageVersion%
 %NuGet% pack ApprovalTests\ApprovalTests.csproj -OutputDirectory .\ApprovalTests -Symbols -Build -Properties Configuration=Debug -Version %PackageVersion%
