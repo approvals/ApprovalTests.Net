69feeedd08f9c5117650ca3ae1d6c0b33fdb2d0d
Serhiy Kalinets <kalinets@gmail.com>
2015-05-15 12:55:49 +0300

    Adds reporter for Jenkins

---
+++ ApprovalTests/Reporters/JenkinsReporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ApprovalTests.Core;
+
+namespace ApprovalTests.Reporters
+{
+    public class JenkinsReporter : IEnvironmentAwareReporter
+    {
+        public static readonly JenkinsReporter INSTANCE = new JenkinsReporter();
+
+        public void Report(string approved, string received)
+        {
+            // does nothing
+        }
+
+        public bool IsWorkingInThisEnvironment(string forFile)
+        {
+            return Environment.GetEnvironmentVariable("JENKINS_URL") != null;
+        }
+    }
+}
