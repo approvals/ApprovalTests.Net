955e4e6feaa1c09243a4ab446b4272b9a8378498
Llewellyn Falco <isidore@setgame.com>
2015-08-22 04:23:16 -0700

    DictionaryUtils w/@DionFoster

---
+++ ApprovalUtilities.Tests/Utilities/CollectionUtilsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using ApprovalUtilities.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApprovalUtilities.Tests.Utilities
+{
+    [TestClass]
+    public class CollectionUtilsTest
+    {
+        [TestMethod]
+        public void TestDictionary()
+        {
+            var d = new Dictionary<int, string> {{1, "one"}};
+            Assert.AreEqual("one", d.GetValueOrDefault(1));
+            Assert.AreEqual(null, d.GetValueOrDefault(2));
+
+        }
+    }
+}
--- ApprovalUtilities/Utilities/CollectionUtilities.cs
+++ ApprovalUtilities/Utilities/CollectionUtilities.cs
@@ -3,22 +3,35 @@ using System.Collections.Generic;
 
 namespace ApprovalUtilities.Utilities
 {
-	public static class CollectionUtilities
-	{
-		public static ICollection<T> AddAll<T>(this ICollection<T> collection, IEnumerable<T> additions)
-		{
-			foreach (var addition in additions)
-			{
-				collection.Add(addition);
-			}
-			return collection;
-		}
-		public static void ForEach<T>(this IEnumerable<T> ie, Action<T> action)
-		{
-			foreach (var i in ie)
-			{
-				action(i);
-			}
-		}
-	}
+    public static class CollectionUtilities
+    {
+        public static ICollection<T> AddAll<T>(this ICollection<T> collection, IEnumerable<T> additions)
+        {
+            foreach (var addition in additions)
+            {
+                collection.Add(addition);
+            }
+            return collection;
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> ie, Action<T> action)
+        {
+            foreach (var i in ie)
+            {
+                action(i);
+            }
+        }
+
+        public static V GetValueOrDefault<K, V>(this IDictionary<K, V> map, K key)
+        {
+            if (map.ContainsKey(key))
+            {
+                return map[key];
+            }
+            else
+            {
+                return default(V);
+            }
+        }
+    }
 }
