14daf0c247c389be8343e1fa19badba5c05bc704
Jason Jarrett <jason@elegantcode.com>
2015-08-27 16:35:10 -0700

    update GetFullOsNameFromWmi to return a valid os name for Mac/Linux

--- ApprovalTests/Namers/ApprovalResults.cs
+++ ApprovalTests/Namers/ApprovalResults.cs
@@ -42,13 +42,28 @@ namespace ApprovalTests.Namers
 
 		private static string GetFullOsNameFromWmi()
 		{
-			var caption =
-				(from x in
-					 new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem").Get().OfType<ManagementObject>()
-				 select x.GetPropertyValue("Caption")).FirstOrDefault();
+			var platformID = Environment.OSVersion.Platform;
+			if (platformID == PlatformID.MacOSX || platformID == PlatformID.Unix) {
+				if (Directory.Exists("/Applications") &&
+					Directory.Exists("/Users") &&
+					Directory.Exists("/Volumes") &&
+					Directory.Exists("/System"))
+				{
+					return "Mac";
+				}
+				else
+				{
+					return "Linux";
+				}
+			} else {
+				var caption =
+					(from x in
+					     new ManagementObjectSearcher ("SELECT Caption FROM Win32_OperatingSystem").Get ().OfType<ManagementObject> ()
+					 select x.GetPropertyValue ("Caption")).FirstOrDefault ();
 
-			var name = caption == null ? Environment.OSVersion.ToString() : caption.ToString();
-			return name;
+				var name = caption == null ? Environment.OSVersion.ToString () : caption.ToString ();
+				return name;
+			}
 		}
 
 		public static string TransformEasyOsName(string captionName)
