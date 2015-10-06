ead3b0ab0941cc4b4d13eeac2a742924a58276cf
Ackara <ackara.dev@outlook.com>
2015-10-05 20:30:13 -0400

    setup my test environment.

---
+++ Ackara/UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApprovalTests.Reporters;
+using ApprovalTests;
+
+namespace Ackara.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [UseReporter(typeof(FileLauncherReporter))]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
