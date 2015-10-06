7ef5819966f080f9dd4b1e6d9511350a7c397d69
Ackara <ackara.dev@outlook.com>
2015-10-06 06:55:44 -0400

    new: add approval writer for data driven test as well as respective unit test.

---
+++ Ackara/DDTWriter.cs
@@ -0,0 +1,38 @@
+﻿using ApprovalTests;
+using ApprovalTests.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ackara
+{
+    public class DDTWriter : ApprovalTextWriter
+    {
+        public DDTWriter(string data, TestContext context)
+            : base(data)
+        {
+            _context = context;
+        }
+
+        public override string GetReceivedFilename(string basename)
+        {
+            return String.Format("{0}.received[{1}]{2}", basename, GetRowNumber(), ExtensionWithDot);
+        }
+
+        public override string GetApprovalFilename(string basename)
+        {
+            return String.Format("{0}.approved[{1}]{2}", basename,  GetRowNumber(), ExtensionWithDot);
+        }
+
+        private TestContext _context;
+
+        public int GetRowNumber()
+        {
+            return _context.DataRow.Table.Rows.IndexOf(_context.DataRow);
+        }
+    }
+}
--- Ackara/UnitTest/UnitTest1.cs
+++ Ackara/UnitTest/UnitTest1.cs
@@ -8,11 +8,24 @@ namespace Ackara.UnitTest
     [TestClass]
     public class UnitTest1
     {
+        public const string
+
+            ODBC = "System.Data.Odbc",
+            ExcelConnectionString = @"Dsn=Excel Files;dbq=C:\Users\Ackeem\OneDrive\Projects\Libraries\C#\ApprovalTests.Net\Ackara\Data\sampleData.xlsx";
+
+        public TestContext TestContext { get; set; }
+
         [TestMethod]
-        [UseReporter(typeof(FileLauncherReporter))]
+        [UseReporter(typeof(DiffReporter))]
+        [DataSource(ODBC, ExcelConnectionString, "Sheet1$", DataAccessMethod.Random)]
         public void TestMethod1()
         {
-            
+            var sample = Convert.ToString(TestContext.DataRow[0]);
+            var table = TestContext.DataRow.Table;
+            var idx = new DDTWriter("", TestContext).GetRowNumber();
+            System.Diagnostics.Debug.WriteLine(idx + " " + sample);
+
+            Approvals.Verify(new DDTWriter(sample, TestContext));
         }
     }
 }
