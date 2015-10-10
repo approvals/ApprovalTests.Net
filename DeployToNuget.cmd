ed4edd27ad4715b31fdc5bb9de3d5ad16ce479ed
Llewellyn Falco <isidore@setgame.com>
2015-10-10 04:59:29 -0700

    v10

--- DeployToNuget.cmd
+++ DeployToNuget.cmd
@@ -1,9 +1,10 @@
 rem .nuget\NuGet.exe setapikey e39ea-get-the-full-key-on-nuget.org
 
 call CreateNuget.cmd
-.nuget\NuGet.exe push nuget_packages\ApprovalTests.3.0.?.nupkg
-.nuget\NuGet.exe push nuget_packages\ApprovalTests.3.0.?.symbols.nupkg
+.nuget\NuGet.exe push nuget_packages\ApprovalUtilities.3.0.??.nupkg
+.nuget\NuGet.exe push nuget_packages\ApprovalUtilities.3.0.??.symbols.nupkg
 
-.nuget\NuGet.exe push nuget_packages\ApprovalUtilities.3.0.?.nupkg
-.nuget\NuGet.exe push nuget_packages\ApprovalUtilities.3.0.?.symbols.nupkg
+.nuget\NuGet.exe push nuget_packages\ApprovalTests.3.0.??.nupkg
+.nuget\NuGet.exe push nuget_packages\ApprovalTests.3.0.??.symbols.nupkg
+
 pause 
