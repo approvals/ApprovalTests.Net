a20f6e7419962be03004220ec28b55d0891f4ba1
Llewellyn Falco <isidore@setgame.com>
2016-03-13 12:29:56 -0700

    Added ReprotWithoutFrontLoading just in case

---
+++ ApprovalTests/Reporters/ReportWithoutFrontLoading.cs
@@ -0,0 +1,17 @@
+﻿using ApprovalTests.Core;
+
+namespace ApprovalTests.Reporters
+{
+    public class ReportWithoutFrontLoading : IEnvironmentAwareReporter
+    {
+        public void Report(string approved, string received)
+        {
+            // do nothing
+        }
+
+        public bool IsWorkingInThisEnvironment(string forFile)
+        {
+            return false;
+        }
+    }
+}
