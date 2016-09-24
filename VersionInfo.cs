ee635d780756a68c397938c9924c22156d4429ea
Jim Counts <jamesrcounts@outlook.com>
2016-09-23 20:51:07 -0700

    Better version specifications

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -3,3 +3,4 @@
 
 [assembly: AssemblyVersion("3.0")]     // Don't change this
 [assembly: AssemblyFileVersion("3.0.12")] // Change this
+[assembly: AssemblyInformationalVersion("3.0.12")] // Change this too
