f55f1e9e997928a18de72f78225677c51422c0fd
Aaron Sherber <aaron@sherber.com>
2017-01-13 20:05:32 -0500

    Add SetApprovals.cs

---
+++ ApprovalTests/Set/SetApprovals.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ApprovalTests.Set
+{
+    public static class SetApprovals
+    {
+        private static IEnumerable<T> GetSorted<T>(IEnumerable<T> enumerable) where T:IComparable<T>
+        {
+            return enumerable.OrderBy(e => e);
+        }
+
+        public static void VerifySet<T>(IEnumerable<T> enumerable, Func<T, string> formatter) where T : IComparable<T>
+        {
+            Approvals.VerifyAll(GetSorted(enumerable), formatter);
+        }
+
+        public static void VerifySet<T>(IEnumerable<T> enumerable, string label) where T : IComparable<T>
+        {
+            Approvals.VerifyAll(GetSorted(enumerable), label);
+        }
+
+        public static void VerifySet<T>(IEnumerable<T> enumerable, string label, Func<T, string> formatter) where T : IComparable<T>
+        {
+            Approvals.VerifyAll(GetSorted(enumerable), label, formatter);
+        }
+
+        public static void VerifySet<T>(string header, IEnumerable<T> enumerable, string label) where T : IComparable<T>
+        {
+            Approvals.VerifyAll(header, GetSorted(enumerable), label);
+        }
+
+        public static void VerifySet<T>(string header, IEnumerable<T> enumerable, Func<T, string> formatter) where T : IComparable<T>
+        {
+            Approvals.VerifyAll(header, GetSorted(enumerable), formatter);
+        }
+
+        public static void VerifyFileAsSet(string filename, Func<string, string> scrubber)
+        {
+            var lines = File.ReadAllLines(filename);
+            var scrubbed = lines.Select(l => scrubber(l));
+            VerifySet(scrubbed, s => s);  
+        }
+
+        public static void VerifyFileAsSet(string filename)
+        {
+            VerifyFileAsSet(filename, ApprovalTests.Scrubber.ScrubberUtils.NO_SCRUBBER);
+        }
+    }
+}
