befc2bd7cd3a6ac73fbbec3f588386df1c1f666a
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-02-22 16:27:15 +0100

    Add support for TortoiseGitMerge as Reporter

---
+++ ApprovalTests/Reporters/TortoiseGitTextDiffReporter.cs
@@ -0,0 +1,11 @@
+﻿namespace ApprovalTests.Reporters
+{
+    public class TortoiseGitTextDiffReporter : GenericDiffReporter
+    {
+        public static readonly TortoiseGitTextDiffReporter INSTANCE = new TortoiseGitTextDiffReporter();
+
+        public TortoiseGitTextDiffReporter() : base(DiffPrograms.Windows.TORTOISEGIT_TEXT_DIFF)
+        {
+        }
+    }
+}
