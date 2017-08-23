61a2350635ae7a206950d1b683b0d0058a8fc178
Simon Cropp <simon.cropp@gmail.com>
2017-08-23 16:12:55 +1000

    only run machine specific tests on allowed machine list

--- ApprovalTests.MachineSpecific.Tests/ApprovalTestsConfig.cs
+++ ApprovalTests.MachineSpecific.Tests/ApprovalTestsConfig.cs
@@ -1,3 +1,21 @@
-﻿using ApprovalTests.Reporters;
+﻿using System;
+using System.Linq;
+using ApprovalTests.Reporters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [assembly: UseReporter(typeof(DiffReporter))]
+
+[TestClass]
+public class SetupAssemblyInitializer
+{
+    [AssemblyInitialize]
+    public static void AssemblyInit(TestContext context)
+    {
+        var machinesToRun = new[]{ "LLEWELLYN-PC" };
+
+        if (!machinesToRun.Contains(Environment.MachineName))
+        {
+            Assert.Inconclusive($"Machine name '{Environment.MachineName}' not in allowed list: {string.Join(", ", machinesToRun)}. See ApprovalTestsConfig.cs");
+        }
+    }
+}
