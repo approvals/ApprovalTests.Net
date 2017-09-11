d850fcdfc431ff39f0abc73fb1a838881f649d51
Simon Cropp <simon.cropp@gmail.com>
2017-09-11 16:24:59 +1000

    fix dir

--- ApprovalTests.MachineSpecific.Tests/SetUpFixture.cs
+++ ApprovalTests.MachineSpecific.Tests/SetUpFixture.cs
@@ -10,11 +10,16 @@ public class SetUpFixture
     [OneTimeSetUp]
     public void SetUp()
     {
+        FixCurrentDirectory();
         var machinesToRun = new[] { "LLEWELLYN-PC" };
 
         if (!machinesToRun.Contains(Environment.MachineName))
         {
             Assert.Inconclusive($"Machine name '{Environment.MachineName}' not in allowed list: {string.Join(", ", machinesToRun)}. See ApprovalTestsConfig.cs");
         }
+    }
+    void FixCurrentDirectory([CallerFilePath] string callerFilePath = "")
+    {
+        Environment.CurrentDirectory = Directory.GetParent(callerFilePath).FullName;
     }
 }
