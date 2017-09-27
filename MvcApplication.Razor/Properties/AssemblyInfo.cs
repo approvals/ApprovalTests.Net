ff9f71e50a4f385628247b1c5a63d69d94f137d9
Llewellyn Falco <isidore@setgame.com>
2017-09-26 21:25:57 -0700

    Removed Mvc project (in Approvals.Net.Asp )
    @SimonCropp

--- MvcApplication.Razor/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MvcApplication1")]
-[assembly: AssemblyCompany("Active Solution AB")]
-[assembly: AssemblyProduct("MvcApplication1")]
--- MvcApplication.Razor/Views/Config.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MvcApplication1.Views
-{
-    public class Config
-    {
-        public const string DefaultLayout = "~/Views/Shared/_Layout.cshtml";
-    }
-}
