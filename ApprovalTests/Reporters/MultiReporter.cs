8bb37b2d606bc0098247d7bf7d0b54d041f02658
Llewellyn Falco <isidore@setgame.com>
2017-10-06 05:52:01 -0700

    Better MultiReporters w/@lars-erik

--- ApprovalTests/Reporters/MultiReporter.cs
+++ ApprovalTests/Reporters/MultiReporter.cs
@@ -1,42 +1,56 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using ApprovalTests.Core;
 
 namespace ApprovalTests.Reporters
 {
-	public class MultiReporter : IApprovalFailureReporter
-	{
-		private readonly IEnumerable<IApprovalFailureReporter> reporters;
+    public class MultiReporter : IEnvironmentAwareReporter, IApprovalReporterWithCleanUp
+    {
+        public bool IsWorkingInThisEnvironment(string forFile)
+        {
+            return Reporters.OfType<IEnvironmentAwareReporter>().Any(r => r.IsWorkingInThisEnvironment(forFile));
+        }
 
-		public MultiReporter(params IApprovalFailureReporter[] reporters)
-		{
-			this.reporters = reporters;
-		}
-		public MultiReporter(IEnumerable<IApprovalFailureReporter> reporters)
-		{
-			this.reporters = reporters;
-		}
+        public void CleanUp(string approved, string received)
+        {
+            foreach (var cleaner in Reporters.OfType<IApprovalReporterWithCleanUp>())
+            {
+                cleaner.CleanUp(approved, received);
+            }
+        }
 
-		public void Report(string approved, string received)
-		{
-			Exception lastThrown = null;
+        public IEnumerable<IApprovalFailureReporter> Reporters { get; }
 
-			foreach (var reporter in reporters)
-			{
-				try
-				{
-					reporter.Report(approved, received);
+        public MultiReporter(params IApprovalFailureReporter[] reporters)
+        {
+            this.Reporters = reporters;
+        }
 
-				}
-				catch (Exception e)
-				{
-					lastThrown = e;
-				}
-			}
-			if (lastThrown != null)
-			{
-				throw lastThrown;
-			}
-		}
-	}
+        public MultiReporter(IEnumerable<IApprovalFailureReporter> reporters)
+        {
+            this.Reporters = reporters;
+        }
+
+        public virtual void Report(string approved, string received)
+        {
+            Exception lastThrown = null;
+
+            foreach (var reporter in Reporters)
+            {
+                try
+                {
+                    reporter.Report(approved, received);
+                }
+                catch (Exception e)
+                {
+                    lastThrown = e;
+                }
+            }
+            if (lastThrown != null)
+            {
+                throw lastThrown;
+            }
+        }
+    }
 }
