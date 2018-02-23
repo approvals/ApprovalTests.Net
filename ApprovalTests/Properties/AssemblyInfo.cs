52abeaa63c96d6ad01a8ec9769021a280f818e7a
Simon Cropp <simon.cropp@gmail.com>
2018-02-23 19:53:24 +1100

    build approval tests nuget from csproj

--- ApprovalTests/Properties/AssemblyInfo.cs
+++ ApprovalTests/Properties/AssemblyInfo.cs
@@ -1,22 +1,3 @@
 ﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("ApprovalTests")]
-[assembly: AssemblyDescription("Approval testing library")]
-[assembly: AssemblyProduct("ApprovalTests")]
 [assembly: CLSCompliant(true)]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("1e5f2ef5-a8d1-46f3-8f14-28ac58b89648")]
--- CreateNuget.cmd
+++
@@ -1,6 +0,0 @@
-set approval_test_version=3.0.13
-if not exist .\nuget_packages mkdir nuget_packages
-del /Q .\nuget_packages\*.*
-.nuget\NuGet.exe pack ApprovalUtilities\ApprovalUtilities.csproj -OutputDirectory .\nuget_packages -Version %approval_test_version% -symbols
-.nuget\NuGet.exe pack ApprovalTests\ApprovalTests.csproj -OutputDirectory .\nuget_packages -Version %approval_test_version% -symbols
-pause 
