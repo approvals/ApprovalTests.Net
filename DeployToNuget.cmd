a720db9159efae6f0528d4b8fda6d893b79cc8f5
Llewellyn Falco <isidore@setgame.com>
2018-03-05 01:58:28 -0800

    added nuget cli

--- DeployToNuget.cmd
+++ DeployToNuget.cmd
@@ -1,9 +1,9 @@
 rem .nuget\NuGet.exe setapikey e39ea-get-the-full-key-on-nuget.org
 
 call CreateNuget.cmd
-.nuget\NuGet.exe push nuget_packages\ApprovalUtilities.3.0.??.nupkg
-.nuget\NuGet.exe push nuget_packages\ApprovalTests.3.0.??.nupkg
-.nuget\NuGet.exe push nuget_packages\ApprovalTests.WinForms.3.0.??.nupkg
-.nuget\NuGet.exe push nuget_packages\ApprovalTests.Rdlc.3.0.??.nupkg
+nuget_cli\NuGet.exe push nuget_packages\ApprovalUtilities.3.0.??.nupkg
+nuget_cli\NuGet.exe push nuget_packages\ApprovalTests.3.0.??.nupkg
+nuget_cli\NuGet.exe push nuget_packages\ApprovalTests.WinForms.3.0.??.nupkg
+nuget_cli\NuGet.exe push nuget_packages\ApprovalTests.Rdlc.3.0.??.nupkg
 
 pause 
