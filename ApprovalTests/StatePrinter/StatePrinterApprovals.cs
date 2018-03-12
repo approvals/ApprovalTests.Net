1a7d5c4d9dba361f9e222dd946da68c13479e876
David De Sloovere <david.desloovere@outlook.com>
2018-03-12 12:41:54 +0100

    Use StatePrinter v3

--- ApprovalTests/StatePrinter/StatePrinterApprovals.cs
+++ ApprovalTests/StatePrinter/StatePrinterApprovals.cs
@@ -1,34 +1,32 @@
 ﻿using System;
-using StatePrinter.Configurations;
+using StatePrinting.Configurations;
 
 namespace ApprovalTests.StatePrinter
 {
-	public static class StatePrinterApprovals
-	{
-		private static Func<Configuration> defaultConfiguration = ConfigurationHelper.GetStandardConfiguration;
+    public static class StatePrinterApprovals
+    {
+        private static Func<Configuration> defaultConfiguration = () => ConfigurationHelper.GetStandardConfiguration(null);
 
-		public static void Verify(object source, string rootName = "Root")
-		{
-			Verify(source, GetDefaultConfiguration(), rootName);
-		}
+        public static void Verify(object source, string rootName = "Root")
+        {
+            Verify(source, GetDefaultConfiguration(), rootName);
+        }
 
+        public static void Verify(object source, Configuration configuration, string rootName = "Root")
+        {
+            var printer = new StatePrinting.Stateprinter(configuration);
+            string printedResult = printer.PrintObject(source, rootName);
+            Approvals.Verify(printedResult);
+        }
 
-		public static void Verify(object source, Configuration configuration, string rootName = "Root")
-		{
-			var printer = new global::StatePrinter.StatePrinter(configuration);
-			string printedResult = printer.PrintObject(source, rootName);
-			Approvals.Verify(printedResult);
-		}
+        public static Configuration GetDefaultConfiguration()
+        {
+            return defaultConfiguration();
+        }
 
-		public static Configuration GetDefaultConfiguration()
-		{
-			return defaultConfiguration();
-		}
-
-		public static void RegisterDefaultConfiguration(Func<Configuration> configurationProducer)
-		{
-			defaultConfiguration = configurationProducer;
-		}
-
-	}
+        public static void RegisterDefaultConfiguration(Func<Configuration> configurationProducer)
+        {
+            defaultConfiguration = configurationProducer;
+        }
+    }
 }
