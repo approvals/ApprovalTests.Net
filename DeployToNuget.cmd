710726ad4621fb864fd1f851ab8045e54f5c9f5c
Llewellyn Falco <isidore@setgame.com>
2018-03-22 01:11:39 -0700

    script

--- DeployToNuget.cmd
+++ DeployToNuget.cmd
@@ -1,9 +1,8 @@
-rem .nuget\NuGet.exe setapikey e39ea-get-the-full-key-on-nuget.org
+rem nuget_cli\NuGet.exe setapikey e39ea-get-the-full-key-on-nuget.org
 
-call CreateNuget.cmd
-nuget_cli\NuGet.exe push nuget_packages\ApprovalUtilities.3.0.??.nupkg
-nuget_cli\NuGet.exe push nuget_packages\ApprovalTests.3.0.??.nupkg
-nuget_cli\NuGet.exe push nuget_packages\ApprovalTests.WinForms.3.0.??.nupkg
-nuget_cli\NuGet.exe push nuget_packages\ApprovalTests.Rdlc.3.0.??.nupkg
+nuget_cli\NuGet.exe push nuget_packages\ApprovalUtilities.3.0.??.nupkg -Source nuget.org
+nuget_cli\NuGet.exe push nuget_packages\ApprovalTests.3.0.??.nupkg -Source nuget.org
+nuget_cli\NuGet.exe push nuget_packages\ApprovalTests.WinForms.3.0.??.nupkg -Source nuget.org
+nuget_cli\NuGet.exe push nuget_packages\ApprovalTests.Rdlc.3.0.??.nupkg -Source nuget.org
 
 pause 
