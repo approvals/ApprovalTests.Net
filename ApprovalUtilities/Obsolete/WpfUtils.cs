30bf1745991a883f1dc2552996902eabb03d09b3
Simon Cropp <simon.cropp@gmail.com>
2018-06-27 16:44:15 +1000

    move wpf utils to wpf package

---
+++ ApprovalUtilities/Obsolete/WpfUtils.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ApprovalUtilities.Wpf
+{
+    [Obsolete(ObsoleteError, true)]
+    public class WpfUtils
+    {
+        const string ObsoleteError = "This class has been moved to the ApprovalTests.Wpf NuGet package (https://www.nuget.org/packages/ApprovalTests.Wpf)";
+    }
+}
