d382fc4868eebce45c610c913aa98d9d636d0a24
Simon Cropp <simon.cropp@gmail.com>
2018-07-11 22:49:22 +1000

    spelling

--- ApprovalTests/Email/EmailApprovals.cs
+++ ApprovalTests/Email/EmailApprovals.cs
@@ -42,16 +42,16 @@ namespace ApprovalTests.Email
 
         private static string ScrubBoundaries(string emailText)
         {
-            var boundies = FindBoundaries(emailText);
-            emailText = ScrubBoundaries(emailText, boundies);
+            var boundaries = FindBoundaries(emailText);
+            emailText = ScrubBoundaries(emailText, boundaries);
             return emailText;
         }
 
-        private static string ScrubBoundaries(string emailText, string[] boundies)
+        private static string ScrubBoundaries(string emailText, string[] boundaries)
         {
             var count = 0;
             var guid = "--boundary_{0}_00000000-0000-0000-0000-00000000000{0}";
-            foreach (var b in boundies)
+            foreach (var b in boundaries)
             {
                 emailText = emailText.Replace(b, string.Format(guid,count++));
             }
@@ -62,16 +62,16 @@ namespace ApprovalTests.Email
         public static string[] FindBoundaries(string emailText)
         {
             var startPoint = 0;
-            var boundies = new HashSet<string>();
+            var boundaries = new HashSet<string>();
             while ((startPoint = emailText.IndexOf("boundary=--", startPoint)) != -1)
             {
                 var preamble = "boundary=--boundary_0_".Length;
                 var guid = "7ddc4a25-b0f6-44d4-bcb0-03f577170c19".Length;
-                boundies.Add(emailText.Substring(preamble + startPoint, guid));
+                boundaries.Add(emailText.Substring(preamble + startPoint, guid));
                 startPoint++;
             }
 
-            return boundies.ToArray();
+            return boundaries.ToArray();
         }
 
         public static string ReadFileWhereLines(string latestFile, Func<string, bool> predicate)
