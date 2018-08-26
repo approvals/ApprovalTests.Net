3ef83f3b31d42c9c40a7501b1322fe322a513bb5
Simon Cropp <simon.cropp@gmail.com>
2018-08-26 23:35:29 +1000

    split out appconfig nuget (#243)

---
+++ ApprovalTests/Obsolete/AppConfigReporter.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace ApprovalTests.Obsolete
+{
+    [Obsolete("This class has been moved to the ApprovalTests.AppConfig NuGet package (https://www.nuget.org/packages/ApprovalTests.AppConfig)", true)]
+    public class AppConfigReporter
+    {
+    }
+}
