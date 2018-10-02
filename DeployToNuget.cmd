8bb8b6ee68b03598bc8886fb648e43acd1f2a5dc
Llewellyn Falco <isidore@setgame.com>
2018-10-02 16:30:29 -0700

    version

--- DeployToNuget.cmd
+++ DeployToNuget.cmd
@@ -1,4 +1,6 @@
 rem nuget_cli\NuGet.exe setapikey e39ea-get-the-full-key-on-nuget.org
+rem increment version in Directory.Build.props
+rem delete nuget_packages and rebuild
 
 nuget_cli\NuGet.exe push nuget_packages\ApprovalUtilities.3.0.??.nupkg -Source nuget.org
 nuget_cli\NuGet.exe push nuget_packages\ApprovalTests.3.0.??.nupkg -Source nuget.org
