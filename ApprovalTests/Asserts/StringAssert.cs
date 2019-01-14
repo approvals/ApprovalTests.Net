abff90221c98e0295ec06842c16bf29b23d7dcf4
Philippe Miossec <pmiossec@gmail.com>
2019-01-14 10:47:56 +0100

    Add (and adapt) string assert from xunit project
    which is under Apache 2 license (like ApprovalTests)

---
+++ ApprovalTests/Asserts/StringAssert.cs
@@ -0,0 +1,131 @@
+﻿namespace ApprovalTests.Asserts
+{
+    public class StringAssert
+    {
+        /// <summary>
+        /// Verifies that two strings are equivalent.
+        /// </summary>
+        /// <param name="expected">The expected string value.</param>
+        /// <param name="actual">The actual string value.</param>
+        /// <exception cref="StringEqualException">Thrown when the strings are not equivalent.</exception>
+        public static void Equal(string expected, string actual)
+        {
+            Equal(expected, actual, false);
+        }
+
+        /// <summary>
+        /// Verifies that two strings are equivalent.
+        /// </summary>
+        /// <param name="expected">The expected string value.</param>
+        /// <param name="actual">The actual string value.</param>
+        /// <param name="ignoreCase">If set to <c>true</c>, ignores cases differences. The invariant culture is used.</param>
+        /// <param name="ignoreLineEndingDifferences">If set to <c>true</c>, treats \r\n, \r, and \n as equivalent.</param>
+        /// <param name="ignoreWhiteSpaceDifferences">If set to <c>true</c>, treats spaces and tabs (in any non-zero quantity) as equivalent.</param>
+        /// <exception cref="StringEqualException">Thrown when the strings are not equivalent.</exception>
+        public static void Equal(string expected, string actual, bool ignoreCase = false, bool ignoreLineEndingDifferences = false, bool ignoreWhiteSpaceDifferences = false)
+        {
+            // Start out assuming the one of the values is null
+            var expectedIndex = -1;
+            var actualIndex = -1;
+            var expectedLength = 0;
+            var actualLength = 0;
+
+            if (expected == null)
+            {
+                if (actual == null)
+                    return;
+            }
+            else if (actual != null)
+            {
+                // Walk the string, keeping separate indices since we can skip variable amounts of
+                // data based on ignoreLineEndingDifferences and ignoreWhiteSpaceDifferences.
+                expectedIndex = 0;
+                actualIndex = 0;
+                expectedLength = expected.Length;
+                actualLength = actual.Length;
+
+                while (expectedIndex < expectedLength && actualIndex < actualLength)
+                {
+                    var expectedChar = expected[expectedIndex];
+                    var actualChar = actual[actualIndex];
+
+                    if (ignoreLineEndingDifferences && IsLineEnding(expectedChar) && IsLineEnding(actualChar))
+                    {
+                        expectedIndex = SkipLineEnding(expected, expectedIndex);
+                        actualIndex = SkipLineEnding(actual, actualIndex);
+                    }
+                    else if (ignoreWhiteSpaceDifferences && IsWhiteSpace(expectedChar) && IsWhiteSpace(actualChar))
+                    {
+                        expectedIndex = SkipWhitespace(expected, expectedIndex);
+                        actualIndex = SkipWhitespace(actual, actualIndex);
+                    }
+                    else
+                    {
+                        if (ignoreCase)
+                        {
+                            expectedChar = char.ToUpperInvariant(expectedChar);
+                            actualChar = char.ToUpperInvariant(actualChar);
+                        }
+
+                        if (expectedChar != actualChar)
+                        {
+                            break;
+                        }
+
+                        expectedIndex++;
+                        actualIndex++;
+                    }
+                }
+            }
+
+            if (expectedIndex < expectedLength || actualIndex < actualLength)
+            {
+                throw new StringEqualException(expected, actual, expectedIndex, actualIndex);
+            }
+        }
+
+        static bool IsLineEnding(char c)
+        {
+            return c == '\r' || c == '\n';
+        }
+
+        static bool IsWhiteSpace(char c)
+        {
+            return c == ' ' || c == '\t';
+        }
+
+        static int SkipLineEnding(string value, int index)
+        {
+            if (value[index] == '\r')
+            {
+                ++index;
+            }
+
+            if (index < value.Length && value[index] == '\n')
+            {
+                ++index;
+            }
+
+            return index;
+        }
+
+        static int SkipWhitespace(string value, int index)
+        {
+            while (index < value.Length)
+            {
+                switch (value[index])
+                {
+                    case ' ':
+                    case '\t':
+                        index++;
+                        break;
+
+                    default:
+                        return index;
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ ApprovalTests/Asserts/StringEqualException.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ApprovalTests.Asserts
+{
+    public class StringEqualException : Exception
+    {
+        static readonly Dictionary<char, string> Encodings = new Dictionary<char, string>
+        {
+            {'\r', "\\r"},
+            {'\n', "\\n"},
+            {'\t', "\\t"},
+            {'\0', "\\0"}
+        };
+
+        /// <summary>
+        /// Gets the actual value.
+        /// </summary>
+        public string Actual { get; }
+
+        /// <summary>
+        /// Gets the expected value.
+        /// </summary>
+        public string Expected { get; }
+
+        private string message;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="StringEqualException"/> class for string comparisons.
+        /// </summary>
+        /// <param name="expected">The expected string value</param>
+        /// <param name="actual">The actual string value</param>
+        /// <param name="expectedIndex">The first index in the expected string where the strings differ</param>
+        /// <param name="actualIndex">The first index in the actual string where the strings differ</param>
+        public StringEqualException(string expected, string actual, int expectedIndex, int actualIndex)
+        {
+            Actual = actual;
+            ActualIndex = actualIndex;
+            Expected = expected;
+            ExpectedIndex = expectedIndex;
+        }
+
+        private string UserMessage => "The string are not equal";
+
+        /// <summary>
+        /// Gets the index into the actual value where the values first differed.
+        /// Returns -1 if the difference index points were not provided.
+        /// </summary>
+        public int ActualIndex { get; }
+
+        /// <summary>
+        /// Gets the index into the expected value where the values first differed.
+        /// Returns -1 if the difference index points were not provided.
+        /// </summary>
+        public int ExpectedIndex { get; }
+
+        /// <inheritdoc/>
+        public override string Message => message ?? (message = CreateMessage());
+
+        string CreateMessage()
+        {
+            if (ExpectedIndex == -1)
+                return base.Message;
+
+            var printedExpected = ShortenAndEncode(Expected, ExpectedIndex, '↓');
+            var printedActual = ShortenAndEncode(Actual, ActualIndex, '↑');
+
+            return string.Format(
+                CultureInfo.CurrentCulture,
+                "{1}{0}          {2}{0}Expected: {3}{0}Actual:   {4}{0}          {5}",
+                Environment.NewLine,
+                UserMessage,
+                printedExpected.Item2,
+                printedExpected.Item1 ?? "(null)",
+                printedActual.Item1 ?? "(null)",
+                printedActual.Item2
+            );
+        }
+
+        static Tuple<string, string> ShortenAndEncode(string value, int position, char pointer)
+        {
+            var start = Math.Max(position - 20, 0);
+            var end = Math.Min(position + 41, value.Length);
+            var printedValue = new StringBuilder(100);
+            var printedPointer = new StringBuilder(100);
+
+            if (start > 0)
+            {
+                printedValue.Append("···");
+                printedPointer.Append("   ");
+            }
+
+            for (var idx = start; idx < end; ++idx)
+            {
+                var c = value[idx];
+                var paddingLength = 1;
+
+                if (Encodings.TryGetValue(c, out var encoding))
+                {
+                    printedValue.Append(encoding);
+                    paddingLength = encoding.Length;
+                }
+                else
+                    printedValue.Append(c);
+
+                if (idx < position)
+                    printedPointer.Append(' ', paddingLength);
+                else if (idx == position)
+                    printedPointer.AppendFormat("{0} (pos {1})", pointer, position);
+            }
+
+            if (value.Length == position)
+                printedPointer.AppendFormat("{0} (pos {1})", pointer, position);
+
+            if (end < value.Length)
+                printedValue.Append("···");
+
+            return new Tuple<string, string>(printedValue.ToString(), printedPointer.ToString());
+        }
+    }
+}
