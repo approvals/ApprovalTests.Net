approvals/ApprovalTests.Net

commit c0d03dc7352afa21ebf0f7ae65cb817d4fea838a

Fixed failing tests

ApprovalTests/Reporters/DiffPrograms.cs
@@ -11,13 +11,13 @@ namespace ApprovalTests.Reporters
         public static class Mac
         {
             public static DiffInfo DIFF_MERGE = new DiffInfo("/Applications/DiffMerge.app/Contents/MacOS/DiffMerge",
-                "%s %s --nosplash", TEXT);
+                "{0} {1}--nosplash", TEXT);
             public static DiffInfo BEYOND_COMPARE = new DiffInfo("/Applications/Beyond Compare.app/Contents/MacOS/bcomp",
                 TEXT);
             public static DiffInfo KALEIDOSCOPE = new DiffInfo("/Applications/Kaleidoscope.app/Contents/MacOS/ksdiff",
                 TEXT_AND_IMAGE);
             public static DiffInfo KDIFF3 = new DiffInfo("/Applications/kdiff3.app/Contents/MacOS/kdiff3",
-                "%s %s -m", TEXT);
+                "{0} {1}-m", TEXT);
             public static DiffInfo P4MERGE = new DiffInfo("/Applications/p4merge.app/Contents/MacOS/p4merge",
                 TEXT_AND_IMAGE);
             public static DiffInfo TK_DIFF = new DiffInfo("/Applications/TkDiff.app/Contents/MacOS/tkdiff", TEXT);
@@ -29,7 +29,7 @@ namespace ApprovalTests.Reporters
             public static DiffInfo BEYOND_COMPARE_4 = new DiffInfo("{ProgramFiles}Beyond Compare 4\\BCompare.exe",
              TEXT_AND_IMAGE);
             public static DiffInfo TORTOISE_IMAGE_DIFF = new DiffInfo(
-                "{ProgramFiles}TortoiseSVN\\bin\\TortoiseIDiff.exe", "/left:%s /right:%s", IMAGE);
+                "{ProgramFiles}TortoiseSVN\\bin\\TortoiseIDiff.exe", "/left:{0} /right:{1}", IMAGE);
             public static DiffInfo TORTOISE_TEXT_DIFF = new DiffInfo(
                 "{ProgramFiles}TortoiseSVN\\bin\\TortoiseMerge.exe", TEXT);
 
@@ -38,9 +38,12 @@ namespace ApprovalTests.Reporters
 
             public static DiffInfo WIN_MERGE = new DiffInfo("{ProgramFiles}WinMerge\\WinMergeU.exe", TEXT);
             public static DiffInfo ARAXIS_MERGE = new DiffInfo("{ProgramFiles}Araxis\\Araxis Merge\\Compare.exe", TEXT);
-            public static DiffInfo CODE_COMPARE = new DiffInfo("{ProgramFiles}Devart\\Code Compare\\CodeCompare.exe", TEXT);
-            public static DiffInfo KDIFF3 = new DiffInfo("{ProgramFiles}KDiff3\\kdiff3.exe", TEXT);
-            public static DiffInfo P4MERGE = new DiffInfo("{ProgramFiles}Perforce\\p4merge.exe", TEXT_AND_IMAGE);
+            public static DiffInfo CODE_COMPARE = new DiffInfo("{ProgramFiles}Devart\\Code Compare\\CodeCompare.exe",
+                "/ENVIRONMENT=visualstudio {0} {1}", TEXT);
+            public static DiffInfo KDIFF3 = new DiffInfo("{ProgramFiles}KDiff3\\kdiff3.exe",
+                "{0} {1} -m -o {1}", TEXT);
+            public static DiffInfo P4MERGE = new DiffInfo("{ProgramFiles}Perforce\\p4merge.exe",
+                "{1} {0} {1} {1}", TEXT_AND_IMAGE);
         }
     }
 }

ApprovalTests.Tests/RunMaintenance.cs
@@ -9,7 +9,7 @@ namespace ApprovalTests.Tests
 		[Test]
 		public void EnsureNoAbandonedFiles()
 		{
-			ApprovalMaintenance.VerifyNoAbandonedFiles();
+			ApprovalMaintenance.VerifyNoAbandonedFiles("CustomNamerShouldBeSubstitutable.approved.txt");
 		}
 	}
 }

ApprovalTests/Maintenance/ApprovalMaintenance.cs
@@ -51,11 +51,12 @@ namespace ApprovalTests.Maintenance
 		}
 
 		
-		public static void VerifyNoAbandonedFiles()
+		public static void VerifyNoAbandonedFiles(params String[] ignore)
 		{
 			var path = PathUtilities.GetDirectoryForCaller(1);
 			var assembly = new Caller().Methods.First().Module.Assembly;
 			var files = FindAbandonedFiles(path, assembly);
+		    files = files.Where(f => !ignore.Any(p => f.FullName.Contains(p))).ToArray();
 			if (files.Any())
 			{
 				throw new Exception("The following files have been abandoned:\n" + files.ToReadableString().Replace(",","\n"));

ApprovalTests/Reporters/GenericDiffReporter.cs
@@ -14,7 +14,7 @@ namespace ApprovalTests.Reporters
 {
     public class GenericDiffReporter : IEnvironmentAwareReporter
     {
-        public const string DEFAULT_ARGUMENT_FORMAT = "\"{0}\" \"{1}\"";
+        public const string DEFAULT_ARGUMENT_FORMAT = "{0} {1}";
 
         private static readonly HashSet<string> TEXT_FILE_TYPES = new HashSet<string>
         {
@@ -178,7 +178,12 @@ Recieved {0} ({1}, {2}, {3})"
 
         public LaunchArgs GetLaunchArguments(string approved, string received)
         {
-            return new LaunchArgs(GetDiffProgram(), arguments.FormatWith(received, approved));
+            return new LaunchArgs(GetDiffProgram(), arguments.FormatWith( WrapPath(received), WrapPath(approved)));
+        }
+
+        private string WrapPath(string path)
+        {
+            return '"' + path + '"';
         }
 
 

commit 9fe6b924b0748f4c890cc650b579c63b3d349716

Update DiffPrograms.cs
Fix to a regression introduced in https://github.com/approvals/ApprovalTests.Net/commit/c0d03dc7352afa21ebf0f7ae65cb817d4fea838a

ApprovalTests/Reporters/DiffPrograms.cs
@@ -11,13 +11,13 @@ namespace ApprovalTests.Reporters
         public static class Mac
         {
             public static DiffInfo DIFF_MERGE = new DiffInfo("/Applications/DiffMerge.app/Contents/MacOS/DiffMerge",
-                "{0} {1}--nosplash", TEXT);
+                "{0} {1} --nosplash", TEXT);
             public static DiffInfo BEYOND_COMPARE = new DiffInfo("/Applications/Beyond Compare.app/Contents/MacOS/bcomp",
                 TEXT);
             public static DiffInfo KALEIDOSCOPE = new DiffInfo("/Applications/Kaleidoscope.app/Contents/MacOS/ksdiff",
                 TEXT_AND_IMAGE);
             public static DiffInfo KDIFF3 = new DiffInfo("/Applications/kdiff3.app/Contents/MacOS/kdiff3",
-                "{0} {1}-m", TEXT);
+                "{0} {1} -m", TEXT);
             public static DiffInfo P4MERGE = new DiffInfo("/Applications/p4merge.app/Contents/MacOS/p4merge",
                 TEXT_AND_IMAGE);
             public static DiffInfo TK_DIFF = new DiffInfo("/Applications/TkDiff.app/Contents/MacOS/tkdiff", TEXT);