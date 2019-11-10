44e9c99737adace497e9725e766b2659ec588c49
Simon Cropp <simon.cropp@gmail.com>
2019-11-10 19:20:09 +1100

    Update AllFailingTestsClipboardReporter.cs

--- ApprovalTests/Reporters/AllFailingTestsClipboardReporter.cs
+++ ApprovalTests/Reporters/AllFailingTestsClipboardReporter.cs
@@ -7,6 +7,7 @@ namespace ApprovalTests.Reporters
     public class AllFailingTestsClipboardReporter : IApprovalFailureReporter
     {
         static StringBuilder builder = new StringBuilder();
+        public static readonly AllFailingTestsClipboardReporter INSTANCE = new AllFailingTestsClipboardReporter();
 
         public void Report(string approved, string received)
         {
