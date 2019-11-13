9951d6975c6c9e2fa27c0086494b7c2a3bdf9dcd
Simon Cropp <simon.cropp@gmail.com>
2019-11-13 19:03:09 +1100

    Mark ConsoleUtilities as obsolete
    fixes #306

--- ApprovalTests/Obsolete/ConsoleUtilities.cs
+++ ApprovalTests/Obsolete/ConsoleUtilities.cs
@@ -3,6 +3,9 @@ using System.Diagnostics;
 
 namespace ApprovalTests.Core
 {
+    [ObsoleteEx(
+        RemoveInVersion = "5.0",
+        Message = @"No longer used internally to approval tests. To replace use `Console.WriteLine(warning)` and `Debug.WriteLine(warning)`.")]
     public static class ConsoleUtilities
     {
         public static void WriteLine(string warning)
