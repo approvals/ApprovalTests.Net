c3753a9a69efae2d6f418003c79338bd65cd7b2b
SimonCropp <simon.cropp@gmail.com>
2020-04-10 09:35:21 +1000

    Create DiffToolReporter.cs

---
+++ src/ApprovalTests/Reporters/DiffToolReporter.cs
@@ -0,0 +1,25 @@
+using ApprovalTests.Core;
+using DiffEngine;
+
+namespace ApprovalTests.Reporters
+{
+    public class DiffToolReporter : IEnvironmentAwareReporter
+    {
+        DiffTool diffTool;
+
+        public DiffToolReporter(DiffTool diffTool)
+        {
+            this.diffTool = diffTool;
+        }
+
+        public void Report(string approved, string received)
+        {
+            DiffRunner.Launch(diffTool, received, approved);
+        }
+
+        public bool IsWorkingInThisEnvironment(string forFile)
+        {
+            return DiffTools.IsDetectedFor(diffTool, forFile);
+        }
+    }
+}
