19fa7ab32e1e0982718252ea8e459075da4246fc
Simon Cropp <simon.cropp@gmail.com>
2021-05-28 09:23:25 +1000

    Update EmailTest.cs

--- src/ApprovalTests.Tests/Email/EmailTest.cs
+++ src/ApprovalTests.Tests/Email/EmailTest.cs
@@ -51,7 +51,6 @@ your biggest fan";
         {
             var text = @"Content-Type: multipart/mixed; boundary=--boundary_1_f3c617c1-4388-492c-8e07-0ef9bdb8af93
 
-
 ----boundary_1_f3c617c1-4388-492c-8e07-0ef9bdb8af93
 Content-Type: multipart/alternative; boundary=--boundary_0_283b02d8-4af3-4d14-83bd-6d7181499f83
 
