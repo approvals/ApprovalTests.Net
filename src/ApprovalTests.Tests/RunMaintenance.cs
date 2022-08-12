17fc27c70a9b359f1958d08d9d5075ef2253bcb0
Simon Cropp <simon.cropp@gmail.com>
2022-08-12 14:17:09 +1000

    exclude some more from VerifyNoAbandonedFiles

--- src/ApprovalTests.Tests/RunMaintenance.cs
+++ src/ApprovalTests.Tests/RunMaintenance.cs
@@ -12,14 +12,8 @@ namespace ApprovalTests.Tests;
             ApprovalMaintenance.VerifyNoAbandonedFiles(
                     "CustomNamerShouldBeSubstitutable.approved.txt",
                     "StringEncodingTest.TestUnicode.approved.txt",
-                    "AsyncTests.TestAsyncExceptionFromVoid.Mac.approved.txt",
-                    "AsyncTests.TestAsyncExceptionFromVoid.Microsoft_Windows_10_Education.approved.txt",
-                    "AsyncTests.TestAsyncExceptionFromVoid.Microsoft_Windows_10_Home_N.approved.txt",
-                    "AsyncTests.TestAsyncExceptionFromVoid.Microsoft_Windows_10_Pro.approved.txt",
-                    "AsyncTests.TestAsyncExceptionFromVoid.Microsoft_Windows_Server_2016_Datacenter.approved.txt",
-                    "AsyncTests.TestAsyncExceptionFromVoid.Microsoft_Windows_Server_2019_Datacenter.approved.txt",
-                    "AsyncTests.TestAsyncExceptionFromVoid.Windows_7.approved.txt",
-                    "AsyncTests.TestAsyncExceptionFromVoid.Windows_8.approved.txt"
+                    "AsyncTests.TestAsyncExceptionFromVoid",
+                    "VerifyExceptionWithStacktrace"
                 );
         }
     }
