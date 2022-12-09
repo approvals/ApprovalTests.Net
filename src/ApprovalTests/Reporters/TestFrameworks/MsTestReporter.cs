d2c445d05117b3c1ea8e6dcf46d87434437b867b
Simon Cropp <simon.cropp@gmail.com>
2022-12-09 20:27:36 +1100

    update to MSTest 3 (#659)

--- src/ApprovalTests/Reporters/TestFrameworks/MsTestReporter.cs
+++ src/ApprovalTests/Reporters/TestFrameworks/MsTestReporter.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Reflection;
 using ApprovalTests.StackTraceParsers;
 
 namespace ApprovalTests.Reporters.TestFrameworks;
@@ -8,7 +8,20 @@ public class MsTestReporter : AssertReporter
     public readonly static MsTestReporter INSTANCE = new();
 
     public MsTestReporter()
-        : base("Microsoft.VisualStudio.TestTools.UnitTesting.Assert, Microsoft.VisualStudio.TestPlatform.TestFramework", "AreEqual", VSStackTraceParser.Attribute)
+        : base(
+            assertClass: "Microsoft.VisualStudio.TestTools.UnitTesting.Assert, Microsoft.VisualStudio.TestPlatform.TestFramework",
+            areEqual: "AreEqual",
+            frameworkAttribute: VSStackTraceParser.Attribute)
     {
+    }
+
+    protected override void InvokeEqualsMethod(Type type, string[] parameters)
+    {
+        var bindingFlags = BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static;
+        var areEquals = type
+            .GetMethods(bindingFlags)
+            .Single(_ => _.Name=="AreEqual" && _.ContainsGenericParameters && _.GetParameters().Length == 2);
+        var method = areEquals.MakeGenericMethod(typeof(string));
+        method.Invoke(null, parameters);
     }
 }
