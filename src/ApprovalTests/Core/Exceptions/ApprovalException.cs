ba88570ff50f0231b9ca1bc31f2948d10fde9517
Simon Cropp <simon.cropp@gmail.com>
2024-01-23 10:59:09 +1100

    drop [Serializable] from exceptions

--- src/ApprovalTests/Core/Exceptions/ApprovalException.cs
+++ src/ApprovalTests/Core/Exceptions/ApprovalException.cs
@@ -1,25 +1,10 @@
-using System.Runtime.Serialization;
-
 namespace ApprovalTests.Core.Exceptions;
 
-[Serializable]
-public class ApprovalException : Exception
+public class ApprovalException(string received, string approved) : Exception
 {
-    public ApprovalException(SerializationInfo info, StreamingContext context) : base(info, context)
-    {
-        Approved = info.GetString("Approved");
-        Received = info.GetString("Received");
-    }
+    public string Received { get; } = received;
 
-    public ApprovalException(string received, string approved)
-    {
-        Received = received;
-        Approved = approved;
-    }
-
-    public string Received { get; }
-
-    public string Approved { get; }
+    public string Approved { get; } = approved;
 
     protected bool Equals(ApprovalException other) => string.Equals(Approved, other.Approved) && string.Equals(Received, other.Received);
 
@@ -42,12 +27,4 @@ public class ApprovalException : Exception
     public static bool operator ==(ApprovalException left, ApprovalException right) => Equals(left, right);
 
     public static bool operator !=(ApprovalException left, ApprovalException right) => !Equals(left, right);
-
-    public override void GetObjectData(SerializationInfo info, StreamingContext context)
-    {
-        base.GetObjectData(info, context);
-
-        info.AddValue("Approved", Approved);
-        info.AddValue("Received", Received);
-    }
 }
