9e481b4a5f29b7f912db3b15fae1a13896f00265
Simon Cropp <simon.cropp@gmail.com>
2024-01-23 12:22:07 +1100

    drop [Serializable] from exceptions (#762)
    * drop [Serializable] from exceptions
    
    * drop [Serializable] from exceptions

--- src/ApprovalTests/Core/Exceptions/ApprovalMismatchException.cs
+++ src/ApprovalTests/Core/Exceptions/ApprovalMismatchException.cs
@@ -1,18 +1,7 @@
-using System.Runtime.Serialization;
-
 namespace ApprovalTests.Core.Exceptions;
 
-[Serializable]
-public class ApprovalMismatchException : ApprovalException
+public class ApprovalMismatchException(string received, string approved) :
+    ApprovalException(received, approved)
 {
-    public ApprovalMismatchException(SerializationInfo info, StreamingContext context)
-        : base(info, context)
-    {
-    }
-
-    public ApprovalMismatchException(string received, string approved) : base(received, approved)
-    {
-    }
-
     public override string Message => $"Failed Approval: Received file {Received} does not match approved file {Approved}.";
 }
--- src/ApprovalTests/Core/Exceptions/ApprovalMissingException.cs
+++ src/ApprovalTests/Core/Exceptions/ApprovalMissingException.cs
@@ -1,17 +1,7 @@
-using System.Runtime.Serialization;
-
 namespace ApprovalTests.Core.Exceptions;
 
-[Serializable]
-public class ApprovalMissingException : ApprovalException
+public class ApprovalMissingException(string received, string approved) :
+    ApprovalException(received, approved)
 {
-    public ApprovalMissingException(SerializationInfo info, StreamingContext context) : base(info, context)
-    {
-    }
-
-    public ApprovalMissingException(string received, string approved) : base(received, approved)
-    {
-    }
-
     public override string Message => $"Failed Approval: Approval File \"{Approved}\" Not Found.";
 }
