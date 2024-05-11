3a6227b41ed3d8848cdfde14fe78d57e8d130b60
Simon Cropp <simon.cropp@gmail.com>
2024-05-11 19:29:48 +1000

    Update FrameworkAssertReporter.cs

--- src/ApprovalTests/Reporters/TestFrameworks/FrameworkAssertReporter.cs
+++ src/ApprovalTests/Reporters/TestFrameworks/FrameworkAssertReporter.cs
@@ -5,9 +5,10 @@ public class FrameworkAssertReporter : FirstWorkingReporter
     public static readonly FrameworkAssertReporter INSTANCE = new();
 
     public FrameworkAssertReporter()
-        : base(MsTestReporter.INSTANCE,
-            NUnit4Reporter.INSTANCE,
+        : base(
+            MsTestReporter.INSTANCE,
             NUnit3Reporter.INSTANCE,
+            NUnit4Reporter.INSTANCE,
             XUnit2Reporter.INSTANCE)
     {
     }
